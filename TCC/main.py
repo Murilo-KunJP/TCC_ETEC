b072e45eb334cdd3f5b9b7e871217b1a42b98604
Murilo <murilo23kunccb@gmail.com>
2026-02-21 19:51:02 -0300

    Refactor user login flow with Google account option

--- TCC/main.py
+++ TCC/main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.set_page_config(initial_sidebar_state="collapsed")
+
 #Login de Usuário
 @st.dialog("Alerta!")
 def login_validation(usr, passw):
@@ -13,16 +15,20 @@ with st.form('sign_in'):
     st.divider()
     username = st.text_input("Nome de Usuário")
     password = st.text_input("Senha", type = 'password')
-
     submit_btn = st.form_submit_button(label="Enviar", key=None, type="primary", use_container_width= True)
-    google_btn = st.form_submit_button(label="Use sua Conta do Google", key=None, type="secondary", use_container_width= True, icon=':material/mail:')
-   
+if not st.user.is_logged_in:
+    if st.button(label="Use sua Conta do Google", key=None, type="secondary", use_container_width= True, icon=':material/mail:'):
+        st.login()
+else:
+    st.switch_page("pages/main1.py")
+    if st.button("Log out"):
+        st.logout()
+    st.write(f"Bem vindo(a)!, {st.user.name}!")
     col1, col2, col3, col4 = st.columns(4)
     with col3:
         remember_box = st.checkbox('Salvar usuário')
     with col4:
-        forgot_password = st.html('<p style= margin:7px><a href=''>Esqueci a Senha</a></p>')
-    create_acc_btn = st.form_submit_button(label="Criar uma Conta", key=None, type="secondary", use_container_width= True)
-
+        st.markdown("[Esqueci a Senha](#)")
+    create_acc_btn = st.button(label="Criar uma Conta",type="secondary",use_container_width=True)
 if submit_btn:
     login_validation(username, password)
